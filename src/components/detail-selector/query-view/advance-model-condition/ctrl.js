53090914a1d4cb0675a7aae9d6d04efd490e2b26
JFBOY <450994392@qq.com>
2017-10-17 14:56:09 +0800

    feat:高级查找优化

--- src/components/detail-selector/query-view/advance-model-condition/ctrl.js
+++ src/components/detail-selector/query-view/advance-model-condition/ctrl.js
@@ -1,1 +1,11 @@
-export default class AdvanceModelConditionBox{}
+import { Inject } from 'angular-es-utils';
+
+@Inject()
+export default class AdvanceModelConditionBox {
+
+	$onInit() {}
+
+	setCondition() {
+		console.log(123);
+	}
+}
