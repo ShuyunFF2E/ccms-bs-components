5da22c01af52a47f94cebde7de75357677d9efa7
JF <450994392@qq.com>
2018-06-29 16:08:12 +0800

    fix(CRMEP-3057)

--- src/components/detail-selector/grid.js
+++ src/components/detail-selector/grid.js
@@ -11,10 +11,12 @@ export default class BaseGridCtrl {
     };
 
     initGridOpts(columns) {
-        this.gridWidth = columns.length * 100 + 50;
+        let gridWidth = 50;
         columns.forEach(item => {
             this.fieldParser[item.code] = this.genFieldParser(item);
+            gridWidth += item.width || 150;
         });
+        this.gridWidth = gridWidth;
         this.generateGridColumns(columns);
     }
 
@@ -78,31 +80,31 @@ export default class BaseGridCtrl {
 				<cc-checkbox ng-model="$parent.$ctrl.conditionState.isAllSelected" ng-change="$parent.$ctrl.switchAllSelect()" cc-tooltip="'选中当前条件下所有的数据'" tooltip-placement="bottom-left"></cc-checkbox>
 			</th>
 			${columns.map((item,index) => {
-        const width = (index === columns.length - 1) ? (this.gridWidth > 920 ? '150px' : 'unset') : '150px';
-        return `<th style="width:${width}">
+                const itemWidth = item.width || 150;
+                const width = (index === columns.length - 1) ? (this.gridWidth > 920 ? (itemWidth + 'px') : 'unset') : (itemWidth + 'px');
+                return `<th style="width:${width}">
                     <div class="bs-ellipsis">${item.name||item.code}</div>
                 </th>`;
-      }).join('')}
+            }).join('')}
 		</tr>`;
 
         const columnsDef = [{
             cellTemplate: `<cc-checkbox ng-model="entity.selected" ng-change="$ctrl.switchSelect(entity)"></cc-checkbox>`,
             width: '30px'
         }].concat(columns.map((item, index) => {
-
-            // this.fieldParser[item.code] = this.genFieldParser(item);
-
+            const itemWidth = item.width || 150;
+            const width = (index === columns.length - 1) ? (this.gridWidth > 920 ? (itemWidth + 'px') : 'unset') : (itemWidth + 'px');
             return {
-                cellTemplate: `<div>
+                cellTemplate: `<div class="bs-ellipsis">
                     <span
-                    ng-bind="$ctrl.fieldParser.${item.code}(entity.${item.code})"
-                    title="{{$ctrl.fieldParser.${item.code}(entity.${item.code})}}"
+                        ng-bind="$ctrl.fieldParser.${item.code}(entity.${item.code})"
+                        title="{{$ctrl.fieldParser.${item.code}(entity.${item.code})}}"
                     ></span>
                   </div>`,
                 field: item.code,
                 displayName: item.name || item.code,
                 tooltip: item.tooltip,
-                width: (index === columns.length - 1) ? (this.gridWidth > 920 ? '150px' : 'unset') : '150px'
+                width
             };
         }));
 
@@ -133,9 +135,9 @@ export default class BaseGridCtrl {
 
 
     genFieldParser(field) {
+        field.dynamicConfigs = field.dynamicConfigs || [];
 
         return value => {
-
             if (field.dataType === 'boolean') {
                 if (!isBoolean(value)) return '';
 
