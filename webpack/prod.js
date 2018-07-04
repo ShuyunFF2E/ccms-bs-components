0214b00a15917ff52a1ccbc48e23262c585eb564
JF <450994392@qq.com>
2018-07-04 14:22:05 +0800

    chore:更新build脚本

--- webpack/prod.js
+++ webpack/prod.js
@@ -6,54 +6,72 @@ var commonConfig = require('./common');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-	devtool: 'source-map',
-	entry: {
-		'components': './src/index.js',
-		'components.min': './src/index.js'
-	},
+    devtool: 'source-map',
+    entry: {
+        'components': './src/index.js',
+        'components.min': './src/index.js'
+    },
 
-	output: {
-		path: path.join(__dirname, '../dist'),
-		filename: '[name].js'
-	},
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: '[name].js'
+    },
 
-	externals: {
-		'jquery': 'jquery',
-		'angular': 'angular',
-		'ccms-components': '\'ccms.components\'',
-		// 'angular-resource': '\'angular-resource\'',
-		// 'angular-ui-router': '\'angular-ui-router\'',
-		'angular-es-utils': '\'angular-es-utils\''
-	},
+    externals: {
+        'jquery': 'jquery',
+        'angular': 'angular',
+        'ccms-ep-components': '\'ccms.components\'',
+        // 'angular-resource': '\'angular-resource\'',
+        // 'angular-ui-router': '\'angular-ui-router\'',
+        'angular-es-utils': '\'angular-es-utils\''
+    },
 
-	plugins: [
-		new ExtractTextPlugin('components.css'),
-		new webpack.DefinePlugin({
-			'process.env': {
-				ENV: JSON.stringify('production'),
-				version: JSON.stringify(require('../package.json').version)
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			include: /\.min\.js$/,
-			minimize: true,
-			sourceMap: true
-		}),
-		new webpack.NoEmitOnErrorsPlugin(),
-	],
-	resolve: commonConfig.resolve,
+    plugins: [
+        new ExtractTextPlugin('components.css'),
+        new webpack.DefinePlugin({
+            'process.env': {
+                ENV: JSON.stringify('production'),
+                version: JSON.stringify(require('../package.json').version)
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true,
+            sourceMap: true
+        }),
+        new webpack.NoEmitOnErrorsPlugin(),
+    ],
+    resolve: commonConfig.resolve,
 
-	module: {
-		rules: commonConfig.module.rules.concat({
-			test: /\.(sc|c)ss$/,
-			use: ExtractTextPlugin.extract({
-				fallback: 'style-loader',
-				use: [
-					{ loader: 'css-loader', options: { sourceMap: true } },
-					{ loader: 'postcss-loader', options: { sourceMap: true } },
-					{ loader: 'sass-loader', options: { sourceMap: true } }
-				]
-			})
-		})
-	}
+    module: {
+        rules: commonConfig.module.rules.concat([{
+            test: /\.(sc|c)ss$/,
+            include: /node_modules|src\/assets/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    { loader: 'css-loader', options: { sourceMap: true } },
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } }
+                ]
+            })
+        }, {
+            test: /\.(sc|c)ss$/,
+            exclude: /node_modules|src\/assets/,
+            use: ExtractTextPlugin.extract({
+                use: [{
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            modules: true,
+                            camelCase: true,
+                            localIdentName: '[local]--[hash:base64:8]'
+                        }
+                    },
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } }
+                ]
+            })
+        }])
+    }
 };
