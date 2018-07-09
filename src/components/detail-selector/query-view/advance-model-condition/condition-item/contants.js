bef878862889f97b951297cfd64acc2f3d21a014
JF <450994392@qq.com>
2018-07-09 16:53:33 +0800

    fix(CRMEP-3812):选择器终端-高级搜索中数值型输入框选项缺少运算符“等于”

--- src/components/detail-selector/query-view/advance-model-condition/condition-item/contants.js
+++ src/components/detail-selector/query-view/advance-model-condition/condition-item/contants.js
@@ -4,7 +4,7 @@ export const DictOperators = ['等于'];
 
 export const BooleanOperators = ['等于'];
 
-export const NumberOperators = ['大于', '大于等于', '小于', '小于等于', '不等于', '介于'];
+export const NumberOperators = ['大于', '大于等于', '小于', '小于等于', '等于', '不等于', '介于'];
 
 export const TextOperators = ['包含', '不包含', '等于', '不等于', '以字母开头', '以字母结尾'];
 
