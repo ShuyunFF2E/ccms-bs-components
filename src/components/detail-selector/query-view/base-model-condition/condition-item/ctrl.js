72f2501cf8cedaf00fbe3279bdbef11876875f71
JF <450994392@qq.com>
2018-07-12 12:46:20 +0800

    fix(CRMEP-3849)

--- src/components/detail-selector/query-view/base-model-condition/condition-item/ctrl.js
+++ src/components/detail-selector/query-view/base-model-condition/condition-item/ctrl.js
@@ -1,37 +1,37 @@
 import styles from './index.scss';
 
 export default class ConditionItemCtrl {
-	styles = styles;
+    styles = styles;
 
-	$onInit() {
-		const condition = this.condition;
+    $onInit() {
+        const condition = this.condition;
 
-		// 数字
-		if (condition.dataType === 'number') {
-			condition.value = condition.value || {
-				min: undefined,
-				max: undefined
-			};
-		}
+        // 数字
+        if (condition.dataType === 'number') {
+            condition.value = condition.value || {
+                min: undefined,
+                max: undefined
+            };
+        }
 
-		// 枚举 & 字典
-		else if (condition.dataType === 'enum' ||
-			condition.dataType === 'dict') {
-			condition.dynamicConfigs = condition.dynamicConfigs || [];
-			condition.value = condition.value || [];
-		}
+        // 枚举 & 字典
+        else if (condition.dataType === 'enum' ||
+            condition.dataType === 'dict') {
+            condition.dynamicConfigs = condition.dynamicConfigs || [];
+            condition.value = condition.value || [];
+        }
 
-		// 文本
-		else if (condition.dataType === 'text') {
-			condition.value = [];
-		}
+        // 文本
+        else if (condition.dataType === 'text') {
+            condition.value = condition.value || [];
+        }
 
-		// 日期
-		else if (condition.dataType === 'date') {
-			condition.value = condition.value || {
-				start: undefined,
-				end: undefined
-			};
-		}
-	}
+        // 日期
+        else if (condition.dataType === 'date') {
+            condition.value = condition.value || {
+                start: undefined,
+                end: undefined
+            };
+        }
+    }
 }
