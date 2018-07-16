785e0ff874bd0c3feb1377a4a768e79f6668205e
JF <450994392@qq.com>
2018-07-16 16:15:26 +0800

    chore:发布时同步淘宝源

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,3 +8,10 @@ cp package.json dist/package.json
 cp README.md dist/README.md
 
 npm publish dist
+
+git add .
+git commit -m 'chore(release)'
+
+
+# cnpm sync ccms-components
+curl -X PUT https://npm.taobao.org/sync/ccms-selector-components
