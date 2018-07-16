e5b1f70b5a665769237d87aa309093d44071d19c
JF <450994392@qq.com>
2018-07-16 18:58:16 +0800

    chore:update release script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,21 +2,24 @@
 
 set -e
 
-# npm run build
+npm run build
 
-# cp package.json dist/package.json
-# cp README.md dist/README.md
+cp package.json dist/package.json
+cp README.md dist/README.md
 
-# npm publish dist
+npm publish dist
 
+
+# match version
 line=`grep 'version' package.json`
 version=${line#*\"\:}
 version=${version#*\"}
 version=${version%\"*}
 
+
 git add .
-git commit -m 'chore(release):v${version}'
+git commit -m "chore(release): v${version}"
 
 
 # cnpm sync ccms-components
-# curl -X PUT https://npm.taobao.org/sync/ccms-selector-components
+curl -X PUT https://npm.taobao.org/sync/ccms-selector-components
